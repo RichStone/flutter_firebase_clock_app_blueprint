fbf12427defd2678732e193e848e90ddc8b67a77
RichStone <richard.had@hotmail.de>
2019-09-19 11:40:48 +0200

    add dummy firestore connection

--- lib/clock_widget.dart
+++ lib/clock_widget.dart
@@ -8,34 +8,13 @@ class ClockWidget extends StatefulWidget {
 }
 
 class _ClockWidgetState extends State<ClockWidget> {
-  String _timeString;
-
-  @override
-  void initState() {
-    _timeString = _formatDateTime(DateTime.now());
-    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Text(
-        _timeString,
+        '_timeString',
         style: TextStyle(fontSize: 30),
       ),
     );
-  }
-
-  void _getTime() {
-    final DateTime now = DateTime.now();
-    final String formattedDateTime = _formatDateTime(now);
-    setState(() {
-      _timeString = formattedDateTime;
-    });
-  }
-
-  String _formatDateTime(DateTime dateTime) {
-    return DateFormat('hh:mm:ss').format(dateTime);
   }
 }
--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SecondRoute extends StatelessWidget {
+  String eatingWindowsUser = 'UNCHANGED';
+  
+  void getData() {
+    Firestore.instance
+      .collection('eatingWindows')
+      .document('wosgyv705DcRJynHuPK0')
+      .get()
+      .then((DocumentSnapshot ds) {
+        eatingWindowsUser = ds['userId'];
+        print(eatingWindowsUser);
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +24,9 @@ class SecondRoute extends StatelessWidget {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            Navigator.pop(context);
+            getData();
           },
-          child: Text('Go back!'),
+          child: Text('printo!'),
         ),
       ),
     );
